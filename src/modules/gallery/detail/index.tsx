c8bc83709341e611e5a2d6b130db40481dd7b41d
0x0603 <33259659+0xxcasper@users.noreply.github.com>
2025-07-25 15:54:54 +0700

    update code

--- src/modules/gallery/detail/index.tsx
+++ src/modules/gallery/detail/index.tsx
@@ -70,7 +70,7 @@ const GalleryDetailPage = () => {
       <motion.div
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: 'easeOut' }}
       >
         <Slider {...settings}>
           {banners.map((banner, index) => (
